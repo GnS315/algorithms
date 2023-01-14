12c567896886d8c6c8e891d60242248cfb64ab9f
GnS315 <morozove2000@gmail.com>
2023-01-14 11:41:35 +0500

    fifo

---
+++ fifo.js
@@ -0,0 +1,36 @@
+// fifo - firs in firs out - первый зашел, первый вышел - очередь
+//
+// Работа с очередью включает в себя следующие операции:
+// enqueue — добавление нового элемента в очередь
+// dequeue — удаление первого элемента из очереди
+// size - получить длинны
+// peekFirst - получить первый элемент без удаления
+//
+//
+//
+//
+//
+//
+
+class Queue {
+
+  constructor() {
+    this.queue = []
+  }
+
+  enqueu(item) {
+    this.queue.push(item)
+  }
+
+  dequeue() {
+    this.queue.shift()
+  }
+
+  size() {
+    return this.queue.length
+  }
+
+  peek() {
+    return this.queue[0]
+  }
+}
